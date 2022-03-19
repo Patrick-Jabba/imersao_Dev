feb76b34013fc870da597ec14df3af51a9021d48
Patrick Monteiro Fischer <66699133+Patrick-Jabba@users.noreply.github.com>
2022-03-19 13:33:07 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+var listaFilmes = [
+    "https://upload.wikimedia.org/wikipedia/pt/e/ec/Mononoke_Hime_p%C3%B4ster.png",
+    "https://upload.wikimedia.org/wikipedia/pt/thumb/0/00/Spider-Man_No_Way_Home_poster.jpg/250px-Spider-Man_No_Way_Home_poster.jpg",
+    "https://www.edmtunes.com/wp-content/uploads/2022/03/The-Batman.jpg"
+  ];
+  
+  for (var i = 0; i < listaFilmes.length; i++) {
+    document.write("<img src=" + listaFilmes[i] + ">");
+  }
+  
